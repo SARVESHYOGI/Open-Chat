332f7a3a23ff966c4476f86afc819f07dc722110
SARVESHYOGI <140194642+SARVESHYOGI@users.noreply.github.com>
2025-02-24 14:58:35 +0530

    added models

---
+++ server/index.js
@@ -0,0 +1,28 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import mongoose, { connect } from 'mongoose';
+import dotenv from 'dotenv';
+import connectdb from './config/database.js';
+
+dotenv.config();
+connectdb();
+
+const app = express();
+app.use(cors(
+    {
+        origin: "http://localhost:5173",
+        credentials: true
+    }
+));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+});
